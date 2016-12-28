63dce42698910a1901202759183eab1d30f22bf9
Danny Lee <dannylee@Dannys-MacBook-Pro.local>
2016-12-27 19:51:21 -0800

    LETS DO THIS

---
+++ LifeTime/TimerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimerViewController.h
+//  LifeTime
+//
+//  Created by Danny Lee on 2016-12-27.
+//  Copyright © 2016 DaekunKim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimerViewController : UIViewController
+
+@end
