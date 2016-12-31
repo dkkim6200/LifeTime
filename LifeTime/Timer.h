b1038e3fb7fd1b69cb59f2ff7abdd51f2e0555db
Danny Lee <dannylee@Dannys-MacBook-Pro.local>
2016-12-30 21:19:56 -0800

    제발

---
+++ LifeTime/Timer.h
@@ -0,0 +1,13 @@
+//
+//  Timer.h
+//  LifeTime
+//
+//  Created by Danny Lee on 2016-12-30.
+//  Copyright © 2016 DaekunKim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Timer : NSObject
+
+@end
---
+++ LifeTime/Timer.m
@@ -0,0 +1,13 @@
+//
+//  Timer.m
+//  LifeTime
+//
+//  Created by Danny Lee on 2016-12-30.
+//  Copyright © 2016 DaekunKim. All rights reserved.
+//
+
+#import "Timer.h"
+
+@implementation Timer
+
+@end
