887254fa76990d3acfd6743be9843d613e1fa6e2
Danny Lee <dannylee@Dannys-MacBook-Pro.local>
2017-06-04 00:50:52 -0700

    Pls work 2

---
+++ LifeTime/GraphViewController.h
@@ -0,0 +1,17 @@
+//
+//  GraphViewController.h
+//  LifeTime
+//
+//  Created by Danny Lee on 2017-05-13.
+//  Copyright © 2017 DaekunKim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DemoBaseViewController.h"
+//#import <Charts/Charts.h>
+@import Charts;
+
+@interface GraphViewController : DemoBaseViewController
+//@property (weak, nonatomic) IBOutlet BarChartView *barChartView;
+
+@end
---
+++ LifeTime/GraphViewController.m
@@ -0,0 +1,200 @@
+//
+//  GraphViewController.m
+//  LifeTime
+//
+//  Created by Danny Lee on 2017-05-13.
+//  Copyright © 2017 DaekunKim. All rights reserved.
+//
+
+#import "GraphViewController.h"
+#import "LifeTime-Swift.h"
+//#import <Charts/Charts-Swift.h>
+@import Charts;
+
+@interface GraphViewController () <ChartViewDelegate>
+@property (weak, nonatomic) IBOutlet BarChartView *barChartView;
+
+@property (weak, nonatomic) IBOutlet UISlider *sliderX;
+@property (weak, nonatomic) IBOutlet UISlider *sliderY;
+@property (nonatomic, strong) IBOutlet UITextField *sliderTextX;
+@property (nonatomic, strong) IBOutlet UITextField *sliderTextY;
+
+@end
+
+@implementation GraphViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.title = @"Bar Chart";
+    
+    self.options = @[
+                     @{@"key": @"toggleValues", @"label": @"Toggle Values"},
+                     @{@"key": @"toggleIcons", @"label": @"Toggle Icons"},
+                     @{@"key": @"toggleHighlight", @"label": @"Toggle Highlight"},
+                     @{@"key": @"animateX", @"label": @"Animate X"},
+                     @{@"key": @"animateY", @"label": @"Animate Y"},
+                     @{@"key": @"animateXY", @"label": @"Animate XY"},
+                     @{@"key": @"saveToGallery", @"label": @"Save to Camera Roll"},
+                     @{@"key": @"togglePinchZoom", @"label": @"Toggle PinchZoom"},
+                     @{@"key": @"toggleAutoScaleMinMax", @"label": @"Toggle auto scale min/max"},
+                     @{@"key": @"toggleData", @"label": @"Toggle Data"},
+                     @{@"key": @"toggleBarBorders", @"label": @"Show Bar Borders"},
+                     ];
+    
+    [self setupBarLineChartView:_barChartView];
+    
+    _barChartView.delegate = self;
+    
+    _barChartView.drawBarShadowEnabled = NO;
+    _barChartView.drawValueAboveBarEnabled = YES;
+    
+    _barChartView.maxVisibleCount = 60;
+    
+    ChartXAxis *xAxis = _barChartView.xAxis;
+    xAxis.labelPosition = XAxisLabelPositionBottom;
+    xAxis.labelFont = [UIFont systemFontOfSize:10.f];
+    xAxis.drawGridLinesEnabled = NO;
+    xAxis.granularity = 1.0; // only intervals of 1 day
+    xAxis.labelCount = 7;
+//    xAxis.valueFormatter = [[DayAxisValueFormatter alloc] initForChart:_chartView];
+    
+    NSNumberFormatter *leftAxisFormatter = [[NSNumberFormatter alloc] init];
+    leftAxisFormatter.minimumFractionDigits = 0;
+    leftAxisFormatter.maximumFractionDigits = 1;
+    leftAxisFormatter.negativeSuffix = @" $";
+    leftAxisFormatter.positiveSuffix = @" $";
+    
+    ChartYAxis *leftAxis = _barChartView.leftAxis;
+    leftAxis.labelFont = [UIFont systemFontOfSize:10.f];
+    leftAxis.labelCount = 8;
+    leftAxis.valueFormatter = [[ChartDefaultAxisValueFormatter alloc] initWithFormatter:leftAxisFormatter];
+    leftAxis.labelPosition = YAxisLabelPositionOutsideChart;
+    leftAxis.spaceTop = 0.15;
+    leftAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
+    
+    ChartYAxis *rightAxis = _barChartView.rightAxis;
+    rightAxis.enabled = YES;
+    rightAxis.drawGridLinesEnabled = NO;
+    rightAxis.labelFont = [UIFont systemFontOfSize:10.f];
+    rightAxis.labelCount = 8;
+    rightAxis.valueFormatter = leftAxis.valueFormatter;
+    rightAxis.spaceTop = 0.15;
+    rightAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
+    
+    ChartLegend *l = _barChartView.legend;
+    l.horizontalAlignment = ChartLegendHorizontalAlignmentLeft;
+    l.verticalAlignment = ChartLegendVerticalAlignmentBottom;
+    l.orientation = ChartLegendOrientationHorizontal;
+    l.drawInside = NO;
+    l.form = ChartLegendFormSquare;
+    l.formSize = 9.0;
+    l.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:11.f];
+    l.xEntrySpace = 4.0;
+    
+//    XYMarkerView *marker = [[XYMarkerView alloc]
+//                            initWithColor: [UIColor colorWithWhite:180/255. alpha:1.0]
+//                            font: [UIFont systemFontOfSize:12.0]
+//                            textColor: UIColor.whiteColor
+//                            insets: UIEdgeInsetsMake(8.0, 8.0, 20.0, 8.0)
+//                            xAxisValueFormatter: _chartView.xAxis.valueFormatter];
+//    marker.chartView = _barChartView;
+//    marker.minimumSize = CGSizeMake(80.f, 40.f);
+//    _barChartView.marker = marker;
+    
+    [self slidersValueChanged:nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)updateChartData
+{
+    if (self.shouldHideData)
+    {
+        _barChartView.data = nil;
+        return;
+    }
+    
+    [self setDataCount:_sliderX.value * 15 range:_sliderY.value * 100];
+}
+
+- (void)setDataCount:(int)count range:(double)range {
+    NSMutableArray *yVals = [[NSMutableArray alloc] init];
+    
+    int startingDataIndex = 1;
+    
+    for (int i = startingDataIndex; i < startingDataIndex + count; i++)
+    {
+        double mult = (range + 1);
+        double val = (double) (arc4random_uniform(mult));
+        if (arc4random_uniform(100) < 25) {
+            [yVals addObject:[[BarChartDataEntry alloc] initWithX:i y:val icon: [UIImage imageNamed:@"icon"]]];
+        } else {
+            [yVals addObject:[[BarChartDataEntry alloc] initWithX:i y:val]];
+        }
+    }
+    
+    BarChartDataSet *set1 = nil;
+    if (_barChartView.data.dataSetCount > 0)
+    {
+        set1 = (BarChartDataSet *)_barChartView.data.dataSets[0];
+        set1.values = yVals;
+        [_barChartView.data notifyDataChanged];
+        [_barChartView notifyDataSetChanged];
+    }
+    else
+    {
+        set1 = [[BarChartDataSet alloc] initWithValues:yVals label:@"The year 2017"];
+        [set1 setColors:ChartColorTemplates.material];
+        set1.drawIconsEnabled = NO;
+        
+        NSMutableArray *dataSets = [[NSMutableArray alloc] init];
+        [dataSets addObject:set1];
+        
+        BarChartData *data = [[BarChartData alloc] initWithDataSets:dataSets];
+        [data setValueFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:10.f]];
+        
+        data.barWidth = 0.9f;
+        
+        _barChartView.data = data;
+    }
+}
+
+- (void)optionTapped:(NSString *)key
+{
+    [super handleOption:key forChartView:_barChartView];
+}
+
+#pragma mark - Actions
+
+- (IBAction)slidersValueChanged:(id)sender
+{
+    _sliderTextX.text = [@((int)(_sliderX.value * 15)) stringValue];
+    _sliderTextY.text = [@((int)(_sliderY.value * 100)) stringValue];
+    
+    [self updateChartData];
+}
+
+#pragma mark - ChartViewDelegate
+
+- (void)chartValueSelected:(ChartViewBase * __nonnull)chartView entry:(ChartDataEntry * __nonnull)entry highlight:(ChartHighlight * __nonnull)highlight
+{
+    NSLog(@"chartValueSelected");
+}
+
+- (void)chartValueNothingSelected:(ChartViewBase * __nonnull)chartView
+{
+    NSLog(@"chartValueNothingSelected");
+}
+- (IBAction)pleasework:(UIButton *)sender {
+    NSLog(@"chartValueNothingSelected");
+
+}
+
+@end
+
